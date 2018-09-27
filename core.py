5241761aef6d4fa588f14437cc667dcac255c804
Jake Cover <coverj715@gmail.com>
2018-09-27 19:15:38 +0000

    Initial commit

---
+++ core.py
@@ -0,0 +1,31 @@
+import discord
+import random
+import re
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if re.search('flex', message.content, re.IGNORECASE):
+        print('flexy message recived')
+        selector = random.randint(1,10)
+        if selector <= 5:
+            current_message = await client.send_message(message.channel, 'I sawed this boat in half!', tts=True)
+        if selector > 5:
+            current_message = await client.send_message(message.channel, 'Hi, Phil Swift here for flex tape!', tts=True)
+        await client.delete_message(current_message)
+
+    if re.search('flex tape', message.content, re.IGNORECASE):
+        await client.add_reaction(message, '™')
+
+
+
+client.run("NDk0OTM2MDAwMzYwMDg3NTYz.Do6xPA.6cofu9CfSaKkhYJsa6TzJmrtOhk")
