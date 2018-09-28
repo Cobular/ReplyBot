5854c2a49c707a4a9a458f13a6644267a31806bf
Jake Cover <coverj715@gmail.com>
2018-09-28 03:47:51 +0000

    Added some SQL stuff, not a ton
    Also updated reqirements.txt

--- core.py
+++ core.py
@@ -1,6 +1,11 @@
 import discord
 import random
 import re
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 client = discord.Client()
 
@@ -33,6 +38,16 @@ async def on_message(message):
 
     if re.search('flex tape', message.content, re.IGNORECASE):
         await client.add_reaction(message, '™')
+
+    # Starts the database session
+    cur = conn.cursor()
+
+    cur.execute('INSERT INTO messages (author, message_content, avitar_hash) VALUES (%s, %s, %s)',
+                (message.author.username, message.content, message.author.avitar))
+    cur.execute("SELECT * FROM messages;")
+    cur.fetchone()
+    conn.commit()
+
 
 
 client.run("NDk0OTM2MDAwMzYwMDg3NTYz.Do6xPA.6cofu9CfSaKkhYJsa6TzJmrtOhk")
